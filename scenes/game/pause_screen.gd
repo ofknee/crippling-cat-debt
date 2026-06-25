a86aff5d1a81d4ea8d386ad43cee66695895b6e5
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2026-06-24 17:31:05 -0700

    Working volume slider

--- scenes/game/pause_screen.gd
+++ scenes/game/pause_screen.gd
@@ -3,6 +3,7 @@ class_name PauseScreen
 
 const MS = Global.MapStates
 @export var volume_slider : DefaultSlider
+@export var back_button : DefaultButton
 
 func _ready() -> void:
 	Global.map_state_changed.connect(_on_map_state_changed)
@@ -10,16 +11,25 @@ func _ready() -> void:
 	volume_slider.value_changed.connect(func(new_val:float):
 		Global.settings.master_volume = clampf(new_val, 0.0, 2.0)
 	)
+	back_button.pressed.connect(func():
+		pause(false)
+	)
+
+func pause(_pause:bool=true):
+	if _pause:
+		Global.map_state = MS.PAUSE
+		get_tree().paused = true
+	else:
+		Global.map_state = MS.PLAY
+		get_tree().paused = false
 
 #DEBUG p for pause
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("p") and OS.is_debug_build() and Global.map_state == MS.PLAY:
-		Global.map_state = MS.PAUSE
-		get_tree().paused = true
+		pause()
 		print("PAUSED")
 	elif Input.is_action_just_pressed("p") and OS.is_debug_build() and Global.map_state == MS.PAUSE:
-		Global.map_state = MS.PLAY
-		get_tree().paused = false
+		pause(false)
 		print("PLAYING")
 
 func _on_map_state_changed(new_state:MS) -> void:
--- utilities/player_settings.gd
+++ utilities/player_settings.gd
@@ -4,13 +4,19 @@ class_name PlayerSettings
 @export var master_volume := 1.0 :
 	set(val):
 		master_volume = val
-		var db = clampf(val, .005, 2.0)
-		db = linear_to_db(db)
-		AudioServer.set_bus_volume_db(
-			AudioServer.get_bus_index("Master"),
-			db
-		)
-		print("New db: %s" % db)
+		var idx = AudioServer.get_bus_index("Master")
+		if val > .005:
+			AudioServer.set_bus_mute(idx, false)
+			var db = clampf(val, .005, 2.0)
+			db = linear_to_db(db)
+			AudioServer.set_bus_volume_db(
+				idx,
+				db
+			)
+			#print("New db: %s" % db)
+		else:
+			AudioServer.set_bus_mute(idx, true)
+			#print("Muted")
 @export var game_volume := 1.0
 @export var cutscene_sounds := 1.0
 #@export var show_tutorial := true
